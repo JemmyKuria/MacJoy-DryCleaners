fca4d152feb1d2b387d2009386edc191825f6cd7
Jemmy Kuria <kuriajemmy@gmail.com>
2026-03-18 22:32:39 +0300

    Deployment

--- macjoy/check_env.py
+++
@@ -1,9 +0,0 @@
-import os 
-from dotenv import load_dotenv
-load_dotenv()
-
-print(f"DB_NAME: {os.getenv('DB_NAME')}")
-print(f"DB_USER: {os.getenv('DB_USER')}")
-print(f"DB_PASSWORD: {os.getenv('DB_PASSWORD')}")
-print(f"DB_HOST: {os.getenv('DB_HOST')}")
-print(f"DB_PORT: {os.getenv('DB_PORT')}")
