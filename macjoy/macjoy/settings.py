d47a5f5a80fd1d62e805a71550c0ea9e9e95a40f
Jemmy Kuria <kuriajemmy@gmail.com>
2026-03-24 00:36:07 +0300

    Import necessary packages

--- macjoy/macjoy/settings.py
+++ macjoy/macjoy/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -74,7 +75,6 @@ WSGI_APPLICATION = 'macjoy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-import os
 from dotenv import load_dotenv
 load_dotenv()
 
